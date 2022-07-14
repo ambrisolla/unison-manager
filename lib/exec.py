41f4c7df75b40d2f76edc15d14ca29a854bdb1cd
root <andremuzel@gmail.com>
2022-07-14 19:48:07 +0000

    adding v0.1

--- lib/exec.py
+++ lib/exec.py
@@ -1,22 +1,27 @@
 import re
 import os
 import sys
-import subprocess       as      sb
-from xmlrpc.client      import  boolean
-from lib.load_settings  import  LoadSettings
+import subprocess         as      sb
+from lib.load_settings    import  LoadSettings
+from lib.status_messages  import StatusMessages
 
 def Execute(**kwargs):
   ''' load global settings '''
   settings = LoadSettings(fullpath=kwargs['fullpath'])
   job_name = kwargs['job_name']
+  ''' print message '''
+  message = f'starting Unison job {job_name}'
+  StatusMessages(message=message)
   ''' check if job_name has one or more " " character'''
   if re.search(' ', job_name) or job_name == '':
+    StatusMessages(message=message, status='fail')
     print('erro: Invalid job name!')
     sys.exit(1)
   else:
     ''' job_name validated, checking if profile exists '''
     profile_full_path = settings['unison_profile_directory']
     if not os.path.exists(f'{profile_full_path}/{job_name}/job.prf'):
+      StatusMessages(message=message, status='fail')
       print('error: Profile file doesn\'t exists!')
       sys.exit(1)
     else:
@@ -24,6 +29,7 @@ def Execute(**kwargs):
       ps = ['ps', '-ef']
       list_process = sb.run(ps, stdout=sb.PIPE, stderr=sb.PIPE, text=True)
       if list_process.returncode != 0:
+        StatusMessages(message=message, status='fail')
         print(f'error: error executing command')
         print(f'{str(list_process.stderr)}')
         sys.exit(1)
@@ -31,7 +37,8 @@ def Execute(**kwargs):
         is_running = True in [ bool(re.search(f'/usr/bin/unison profiles/{job_name}/job.prf',x)) \
           for x in list_process.stdout.split('\n') ]
         if is_running:
-          print(f'info: Job {job_name} already is running!')
-        else:
-          os.system(f'/usr/bin/unison profiles/{job_name}/job.prf &')
-          print(f'info: job {job_name} started!')
+          StatusMessages(message=message, status='fail')
+          print(f'error: Job {job_name} already is running!')          
+        else:          
+          os.system(f'/usr/bin/unison profiles/{job_name}/job.prf > /dev/null 2> /dev/null &')
+          StatusMessages(message=message, status='success')
